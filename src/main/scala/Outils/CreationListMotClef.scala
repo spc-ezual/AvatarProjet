7d2fcaaed6b59c5ec6b95d110a1bebfb48a3174f
Marcus <marcusfoin1@gmail.com>
2023-03-24 21:36:21 +0100

    Correction de creationDeRep, UI

--- src/main/scala/Outils/CreationListMotClef.scala
+++ src/main/scala/Outils/CreationListMotClef.scala
@@ -6,7 +6,16 @@ import DB.PolitesseDAO
 object CreationListMotClef {
   val dsbLieux = ListLieuxDAO
   val dsbPolitesse = PolitesseDAO
-  def MatchMotClef(
-      mots: List[String]
-  ): (String, List[(String, String)]) = ???
+  /**
+    * 
+    * @param mots Phrase couper a chaque mots
+    * @return 1ere string: les politesses, List (nom,adresse)
+    */
+  def MatchMotClef(mots: List[String]): (String, List[(String, String)]) = {
+    ("Les politesses",List(("nom1","Adresse1"),("nom2","Adresse2")))
+  }
+
+  def MatchPolitesse(mots : List[String]): String={
+    ???
+  }
 }
