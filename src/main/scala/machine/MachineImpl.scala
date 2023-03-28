2fed14eb2de7b8a3354b3efb362f0ae741e0ba30
Marcus <marcusfoin1@gmail.com>
2023-03-28 11:45:56 +0200

    modif final ?

--- src/main/scala/machine/MachineImpl.scala
+++ src/main/scala/machine/MachineImpl.scala
@@ -1,9 +1,18 @@
 package machine
 
+import Outils.CreationDeRep
+import application.UI
+
 object MachineImpl extends MachineDialogue {
-  def ask(s: String): List[String] = ???
+  def ask(s: String): List[String] = CreationDeRep.Reponse(s)
 
   // Pour la partie test par le client
-  def reinit(): Unit = ???
-  def test(l: List[String]): List[String] = ???
+  def reinit(): Unit = {}
+  def test(l: List[String]): List[String] = {
+    var rep = Nil
+    for( i <- l){
+       rep :: ask(i)
+    }
+    rep
+  }
 }
