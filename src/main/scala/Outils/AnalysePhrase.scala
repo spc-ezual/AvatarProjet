4eeec84aa16e7640589a4fff56756d6f35d0b4be
gregoire bodin <grbodin@d020m07.istic.univ-rennes1.fr>
2023-03-28 11:09:57 +0200

    compartList

--- src/main/scala/Outils/AnalysePhrase.scala
+++ src/main/scala/Outils/AnalysePhrase.scala
@@ -7,9 +7,26 @@ object AnalysePhrase {
   /** @return
     */
   def SepMots(phrase: String): List[String] = {
-  phrase.split("[ ,./:!?<>();_+-={}&#|'']+").toList
-  
+  val mots = phrase.split("[ ,./:!?<>();_+-={}&#|]+")
+  mots.flatMap { mot =>
+    mot match {
+      case motAvecApostrophe if motAvecApostrophe.endsWith("'") =>
+        List(motAvecApostrophe.dropRight(1), "'")
+      case motAvecApostrophe if motAvecApostrophe.contains("'") =>
+        val (avantApostrophe, apresApostrophe) = motAvecApostrophe.splitAt(motAvecApostrophe.indexOf("'")+1)
+        List(avantApostrophe, apresApostrophe)
+      case _ => List(mot)
+    }
+  }.toList
 }
+
+def compartList(lMotsBdd:List[String], lMotsRequet:List[String]): Boolean ={
+        lMotsBdd match {
+            case head :: next => lMotsRequet.contains(head)&&compartList(next,lMotsRequet)
+            case Nil => true
+        }
+    }
+    
 def areEqualIgnoreCaseAndAccents(char1: Char, char2: Char): Boolean = {
             // Convertir les caractères en chaînes de caractères et les normaliser
             val str1 = Normalizer.normalize(char1.toString, Normalizer.Form.NFD)
