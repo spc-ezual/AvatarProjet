0ee91c1624bca97fe54f5f27c8524bdab0e0c85c
steven guerin <stguerin@d020m10.istic.univ-rennes1.fr>
2023-04-11 09:14:02 +0200

    UI remastered

--- src/main/scala/application/UI.scala
+++ src/main/scala/application/UI.scala
@@ -1,8 +1,5 @@
 package application
 
-
-
-
 import scala.swing._
 import java.awt.Color
 import scala.swing.event._
@@ -10,76 +7,81 @@ import Outils.CreationDeRep._
 import javax.swing.ImageIcon
 import machine.MachineImpl
 
-/**
- * MainFrame realizing the CopyThat application
- */
+/** MainFrame realizing the CopyThat application
+  */
 class UI extends MainFrame {
 
-    preferredSize = new Dimension(1000, 1000)
-    title="Avatar"
-    
-    val chatArea= new BoxPanel(Orientation.Vertical)
-    val inputField = new TextField{columns=40}
-    val sendButton = new Button{text = "Send"}
-    val resetButton = new Button{text = "New Conversation"}
-    val avatarIcon= new ImageIcon("Image/Avatar.png")
-    val userIcon = new ImageIcon("Image/User.png")
-    
-    listenTo(sendButton, inputField.keys,resetButton)
-    
-    reactions += {
-        case ButtonClicked(`resetButton`) => 
-            MachineImpl.reinit()
-    
-        case ButtonClicked(`sendButton`) =>
-            
-            val message = inputField.text
-            
-            if(!message.isEmpty()){
-                // Create user message
-                val userMessage = new FlowPanel {
-                    contents += new Label {icon = userIcon}
-                    contents += new Label {text = s"You: $message"}
-                }
-                // Create avatar message
-                val avatarMessage = new FlowPanel {
-                    contents += new Label {icon = avatarIcon}
-                    contents += new Label {text = "Avatar: " + formatReponse(Reponse(message))}
-                }
-                // Add messages to chat area
-                chatArea.contents += userMessage
-                chatArea.contents += avatarMessage
-                inputField.text = ""
-                chatArea.revalidate();
-                chatArea.repaint();
-            }
+  preferredSize = new Dimension(1000, 1000)
+  title = "Avatar"
 
-        case KeyPressed(`inputField`, Key.Enter, _, _) =>
-            
-            val message = inputField.text
-            
-            if(!message.isEmpty()){
-                // Create user message
-                val userMessage = new FlowPanel {
-                    contents += new Label {icon = userIcon}
-                    contents += new Label {text = s"You: $message"}
-                }
-                // Create avatar message
-                val avatarMessage = new FlowPanel {
-                    contents += new Label {icon = avatarIcon}
-                    contents += new Label {text = "Avatar: " + formatReponse(Reponse(message))}
-                }
-                // Add messages to chat area
-                chatArea.contents += userMessage
-                chatArea.contents += avatarMessage
-                inputField.text = ""
-                chatArea.revalidate();
-                chatArea.repaint();
-            }
+  val chatArea = new BoxPanel(Orientation.Vertical)
+  val inputField = new TextField { columns = 40 }
+  val sendButton = new Button { text = "Send" }
+  val resetButton = new Button { text = "New Conversation" }
+  val avatarIcon = new ImageIcon("Image/Avatar.png")
+  val userIcon = new ImageIcon("Image/User.png")
+
+  listenTo(sendButton, inputField.keys, resetButton)
+
+  reactions += {
+    case ButtonClicked(`resetButton`) =>
+      MachineImpl.reinit()
+
+    case ButtonClicked(`sendButton`) =>
+      val message = inputField.text
+
+      if (!message.isEmpty()) {
+        // Create user message
+        val userMessage = new FlowPanel {
+          contents += new Label { icon = userIcon }
+          contents += new Label { text = s"You: $message" }
         }
-        contents= new  BoxPanel(Orientation.Vertical){
-        contents += new ScrollPane(chatArea)
-        contents += new FlowPanel(inputField, sendButton,resetButton)
-    }
-        
+        // Create avatar message
+        val avatarMessage = new FlowPanel {
+          contents += new Label { icon = avatarIcon }
+          contents += new Label {
+            text = "Avatar: " + formatReponse(Reponse(message))
+          }
+        }
+        // Add messages to chat area
+        chatArea.contents += userMessage
+        chatArea.contents += avatarMessage
+        inputField.text = ""
+        chatArea.revalidate();
+        chatArea.repaint();
+      }
+
+    case KeyPressed(`inputField`, Key.Enter, _, _) =>
+      val message = inputField.text
+
+      if (!message.isEmpty()) {
+        // Create user message
+        val userMessage = new FlowPanel {
+          contents += new Label { icon = userIcon }
+          contents += new Label { text = s"You: $message" }
+        }
+        // Create avatar message
+        val avatarMessage = new FlowPanel {
+          contents += new Label { icon = avatarIcon }
+          contents += new Label {
+            text = "Avatar: " + formatReponse(Reponse(message))
+          }
+        }
+        // Add messages to chat area
+        chatArea.contents += userMessage
+        chatArea.contents += avatarMessage
+        inputField.text = ""
+        chatArea.revalidate();
+        chatArea.repaint();
+      }
+  }
+
+  var chat = new ScrollPane(chatArea)
+  chat.preferredSize = new Dimension(1000, 950)
+
+  contents = new BoxPanel(Orientation.Vertical) {
+    contents += chat
+    contents += new FlowPanel(inputField, sendButton, resetButton)
+  }
+
 }
