923ba460fcf193007201945c16dd7a58121b1159
Gregoire <gregoire.bodin@etudiant.univ-rennes1.fr>
2023-04-24 22:22:33 +0200

    test LieuxXMLDOAO

--- src/test/scala/machine/TestLieuxDAO.scala
+++ src/test/scala/machine/TestLieuxDAO.scala
@@ -4,12 +4,24 @@ import org.junit.Assert._
 import DB.LieuxXMLDAO
 import DB.ListLieuxDAO
 
-class TestLieuxDAO{
+class TestLieuxDAO {
 
-    
-    @Test
-    def Test:Unit={
-        assertEquals(LieuxXMLDAO.getAdresseId("2024"),Some("2, Rue Emile Desprès"))
-    }
-    
-} 
+  @Test
+  def Test_getAdresseId1: Unit = {
+    assertEquals(LieuxXMLDAO.getAdresseId("2024"), Some("2, Rue Emile Desprès"))
+  }
+
+  @Test
+  def Test_getAdresseId2: Unit = {
+    assertEquals(LieuxXMLDAO.getAdresseId("0"), None)
+  }
+
+  @Test
+  def Test_getAdresseId3: Unit = {
+    assertEquals(
+      LieuxXMLDAO.getAdresseId("1"),
+      Some("4, Rue Abbe Huet,rue Abbe Huet")
+    )
+  }
+
+}
