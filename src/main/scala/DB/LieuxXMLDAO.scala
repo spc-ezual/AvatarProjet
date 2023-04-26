8b3f7802fb80dad6bd6c4de1c7cb8237caf1a6d1
Gregoire <gregoire.bodin@etudiant.univ-rennes1.fr>
2023-04-26 20:57:52 +0200

    all test LieuxXMLDAO

--- src/main/scala/DB/LieuxXMLDAO.scala
+++ src/main/scala/DB/LieuxXMLDAO.scala
@@ -6,120 +6,121 @@ import java.io.File
 /** Classe permettant d'interagir avec la table ListLieux de la base de données ListLieux.db
   */
 object LieuxXMLDAO {
-    val dbFilePath = "BaseDeDonnees/DataLieuxXML.db"
-    // Vérification que le fichier existe
-    if (!new File(dbFilePath).exists() || !new File(dbFilePath).isFile()) {
-        throw new RuntimeException(
-        "Le fichier de base de données n'existe pas ou n'est pas un fichier valide."
+  val dbFilePath = "BaseDeDonnees/DataLieuxXML.db"
+  // Vérification que le fichier existe
+  if (!new File(dbFilePath).exists() || !new File(dbFilePath).isFile()) {
+    throw new RuntimeException(
+      "Le fichier de base de données n'existe pas ou n'est pas un fichier valide."
+    )
+  }
+
+  /** @param id l'id d'une organisation valide
+    * @return retourne une adresse si elle existe sinon None
+    */
+  def getAdresseId(id: String): Option[String] = {
+    val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
+    val statement: Statement = connection.createStatement()
+    val query: String =
+      s"SELECT street_number,street_name FROM addresses WHERE organization_id= '$id'"
+    val resultSet = statement.executeQuery(query)
+    val rep =
+      if (resultSet.next()) {
+        val street_number = resultSet.getString("street_number")
+        var street_name = resultSet.getString("street_name")
+        if (street_name.length() > 0)
+          street_name = street_name
+            .split(" ")
+            .map(word => word.head.toUpper + word.tail.toLowerCase)
+            .mkString(" ")
+        Some(
+          street_number +
+            ", " + street_name
         )
-    }
-    /**
-      * 
-      *
-      * @param id id d'une organisation valide
-      * @return retourne une adresse si elle existe sinon None
-      */
-    def getAdresseId(id: String): Option[String] = {
-        val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
-        val statement: Statement = connection.createStatement()
-        val query: String = s"SELECT street_number,street_name FROM addresses WHERE organization_id= '$id'"
-        val resultSet=statement.executeQuery(query)
-        val rep =
-            if(resultSet.next()) {
-              val street_number = resultSet.getString("street_number")
-              var street_name = resultSet.getString("street_name")
-              if(street_name.length()>0) street_name = street_name.split(" ").map(word => word.head.toUpper  + word.tail.toLowerCase).mkString(" ")
-            Some(street_number+
-            ", "+street_name)}
-            else None
-        statement.close()
-        connection.close()
-        rep
-    }
-    /**
-      * 
-      *
-      * @param mots Liste non-vide de mot a rechercher dans les name d'organisation
-      * @return retourn l'ensemble des id d'organisation qui contiens noms dans name , None si ensemble vide
-      */
-    def getId(mots: List[String]): Option[List[String]] = {
-        val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
-        val statement: Statement = connection.createStatement()
-        var req: String = "SELECT id FROM organizations WHERE id IN (SELECT organization_id FROM addresses WHERE city = 'Rennes') AND "
-        for(i <- 0 until mots.length){
-            req = req + " name LIKE '%" + mots(i) + "%' "
-            if (i != mots.length-1) req += " AND "
-        }
-        req += " COLLATE utf8_general_ci; "
-        val resultSet=statement.executeQuery(req)
+      } else None
+    statement.close()
+    connection.close()
+    rep
+  }
 
-        var resultList = List[String]()
-        while (resultSet.next()) {
-            resultList = resultList ++ resultSet.getString("id").split(",").map(_.trim).toList
-        }
-
-        resultSet.close()
-        statement.close()
-        connection.close()
-
-        if (resultList.nonEmpty) Some(resultList) else None
-}
-    /**
-      * 
-      *
-      * @param id id d'organisation valide 
-      * @return name de l'organisation si elle existe sinon None
-      */
-    def getNameId(id: String): Option[String] = {
-        val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
-        val statement: Statement = connection.createStatement()
-        val query: String = s"SELECT name FROM organizations WHERE id= '$id'"
-        val resultSet=statement.executeQuery(query)
-        val rep =
-            if(resultSet.next()) Some(resultSet.getString("name"))else None
-        resultSet.close()
-        statement.close()
-        connection.close()
-        rep
+  /** @param mots Liste non-vide de mot a rechercher dans les name d'organisation
+    * @return retourn l'ensemble des id d'organisation qui contiens les mots dans name , None si ensemble vide
+    */
+  def getId(mots: List[String]): Option[List[String]] = {
+    val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
+    val statement: Statement = connection.createStatement()
+    var req: String =
+      "SELECT id FROM organizations WHERE id IN (SELECT organization_id FROM addresses WHERE city = 'Rennes') AND "
+    for (i <- 0 until mots.length) {
+      req = req + " name LIKE '%" + mots(i) + "%' "
+      if (i != mots.length - 1) req += " AND "
     }
-    /**
-      * 
-      *
-      * @return l'ensemble des mots contenue dans les name de maniere unique
-      */
-    def listeMotsUniques(): List[String]={
-        val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
-        val statement: Statement = connection.createStatement()
-        val query: String = s"SELECT name FROM organizations;"
-        val resultSet=statement.executeQuery(query)
-        var result: List[String] = List()
-        while (resultSet.next()) {
-        result = resultSet.getString("name").split("[ ,./:!?<>();_+-={}&#|']+").toList ::: result
-        }
-        resultSet.close()
-        statement.close()
-        connection.close()
-        result.distinct
-        
+    req += " COLLATE utf8_general_ci; "
+    val resultSet = statement.executeQuery(req)
 
+    var resultList = List[String]()
+    while (resultSet.next()) {
+      resultList =
+        resultList ++ resultSet.getString("id").split(",").map(_.trim).toList
     }
-    /**
-      * 
-      *
-      * @return liste de tous les name des organisation
-      */
-    def getNom(): List[String] = {
-        val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
-        val statement = connection.createStatement()
-        val resultSet = statement.executeQuery("SELECT nom FROM organizations")
-        var noms: List[String] = List()
-        while (resultSet.next()) {
-        noms = resultSet.getString("nom") :: noms
-        }
-        resultSet.close()
-        statement.close()
-        connection.close()
-        noms
+
+    resultSet.close()
+    statement.close()
+    connection.close()
+
+    if (resultList.nonEmpty) Some(resultList) else None
+  }
+
+  /** @param id d'organisation valide
+    * @return name de l'organisation si elle existe sinon None
+    */
+  def getNameId(id: String): Option[String] = {
+    val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
+    val statement: Statement = connection.createStatement()
+    val query: String = s"SELECT name FROM organizations WHERE id= '$id'"
+    val resultSet = statement.executeQuery(query)
+    val rep =
+      if (resultSet.next()) Some(resultSet.getString("name")) else None
+    resultSet.close()
+    statement.close()
+    connection.close()
+    rep
+  }
+
+  /** @return l'ensemble des mots contenue dans les name de maniere unique
+    */
+  def listeMotsUniques(): List[String] = {
+    val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
+    val statement: Statement = connection.createStatement()
+    val query: String = s"SELECT name FROM organizations;"
+    val resultSet = statement.executeQuery(query)
+    var result: List[String] = List()
+    while (resultSet.next()) {
+      result = resultSet
+        .getString("name")
+        .split("[ ,./:!?<>();_+-={}&#|']+")
+        .toList ::: result
     }
+    resultSet.close()
+    statement.close()
+    connection.close()
+    result.distinct
+
+  }
+
+  /** @return liste de tous les name des organisation
+    */
+  def getNom(): List[String] = {
+    val connection = DriverManager.getConnection("jdbc:sqlite:" + dbFilePath)
+    val statement = connection.createStatement()
+    val resultSet = statement.executeQuery("SELECT nom FROM organizations")
+    var noms: List[String] = List()
+    while (resultSet.next()) {
+      noms = resultSet.getString("nom") :: noms
+    }
+    resultSet.close()
+    statement.close()
+    connection.close()
+    noms
+  }
 
 }
--- src/test/scala/machine/TestLieuxDAO.scala
+++ src/test/scala/machine/TestLieuxDAO.scala
@@ -24,4 +24,73 @@ class TestLieuxDAO {
     )
   }
 
+  @Test
+  def Test_getAdresseId4: Unit = {
+    assertEquals(
+      LieuxXMLDAO.getAdresseId("2114"),
+      None
+    )
+  }
+
+  @Test
+  def Test_getAdresseId5: Unit = {
+    assertEquals(
+      LieuxXMLDAO.getAdresseId("2115"),
+      Some("1, Avenue Saint Vincent")
+    )
+  }
+
+  @Test
+  def Test_getId1: Unit = {
+    assertEquals(
+      LieuxXMLDAO.getId(List("Université Rennes 3")),
+      None
+    )
+  }
+
+  @Test
+  def Test_getId2: Unit = {
+    assertEquals(LieuxXMLDAO.getId(List("TVR")), Some(List("1173")))
+  }
+
+  @Test
+  def Test_getId3: Unit = {
+    assertEquals(
+      LieuxXMLDAO.getId(List("Tennis", "Gayeulles")),
+      Some(List("515"))
+    )
+  }
+
+  @Test
+  def Test_getId4: Unit = {
+    assertEquals(
+      LieuxXMLDAO.getId(List("Tennis")),
+      Some(List("1114", "1451", "32", "433", "515"))
+    )
+  }
+
+  @Test
+  def Test_getNameId1: Unit = {
+    assertEquals(
+      LieuxXMLDAO.getNameId("2114"),
+      Some("Dor Breizh Info-Trafic agglo")
+    )
+  }
+
+  @Test
+  def Test_getNameId2: Unit = {
+    assertEquals(
+      LieuxXMLDAO.getNameId("0"),
+      None
+    )
+  }
+
+  @Test
+  def Test_getNameId3: Unit = {
+    assertEquals(
+      LieuxXMLDAO.getNameId("1355"),
+      Some("Université de Rennes 1")
+    )
+  }
+
 }
