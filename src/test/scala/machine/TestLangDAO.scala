c5bb8417681fde34a5e7c3bbddb653e23e0f0311
Bruneau Jeanne <jeanne.bruneau@etudiant.univ-rennes1.fr>
2023-05-04 07:46:05 +0000

    Update TestLangDAO.scala

--- src/test/scala/machine/TestLangDAO.scala
+++ src/test/scala/machine/TestLangDAO.scala
@@ -12,12 +12,8 @@ class TestLangDAO{
     @Test
     def Test1:Unit={
         assertEquals (LangDAO.getMotsLang(2),List("español","hola","buenos","dias","donde","esta","busco","buscando"))
-        }
-    @Test
-    def Test1:Unit={
-        assertEquals (LangDAO.getMotsLang(2),List("español","hola","buenos","dias","donde","esta","busco","buscando"))
-        }
-
+     }
+  
     @Test
      def Test2:Unit={
         assertEquals(LangDAO.getMotsLang(4),List("italiano", "buongiorno", "ciao", "salve", "buon", "pomeriggio", "buonasera", "incantato",  "dove", "trova", "cerco", "cercando"))    
