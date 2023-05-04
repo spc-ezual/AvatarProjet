e8a2b2285b2ff6f278e1f1560ce480cfbe9f0f13
BeXyu <simonbdy@gmail.com>
2023-05-04 06:46:15 +0200

    TestCreationRep mise à jour
    Mise à jour des séries de tests pour coller à la nouvelle classe CreationDeRep

--- src/test/scala/machine/TestCreationRep.scala
+++ src/test/scala/machine/TestCreationRep.scala
@@ -6,18 +6,62 @@ import DB.ListLieuxDAO
 import Outils.CreationDeRep
 
 class TestCreationRep {
-  //Jeu de test FR
-    @Test
+  @Test
+    //Test getLangue
+  def test_get_lang_1 {
+    CreationDeRep.langues = 1
+    assertEquals(
+      1,
+      CreationDeRep.getLangue()
+    )
+  }
+  @Test
+    //Test getLangue
+  def test_get_lang_2 {
+    CreationDeRep.langues = 2
+    assertEquals(
+      2,
+      CreationDeRep.getLangue()
+    )
+  }
+  @Test
+    //Test changement de langue
+  def test_lang_1 {
+    CreationDeRep.action = 0
+    CreationDeRep.langues = 0
+    assertEquals(
+      List("Hablas español?"),
+      CreationDeRep.Reponse("donde esta")
+    )
+  }
+  @Test
+    //Test changement de langue quand il attends un lieu
+  def test_lang_2 {
+    CreationDeRep.action = 1
+    CreationDeRep.langues = 0
+    assertEquals(
+      List("Hablas español?"),
+      CreationDeRep.Reponse("donde esta")
+    )
+  }
+
+  //Série de tests pour le Français
+
+  @Test
     //Test avec uniquement bonjour sans autre requête
   def test_politesse_nil {
+    CreationDeRep.action = 1
+    CreationDeRep.langues = 0
     assertEquals(
       List("Bonjour"),
       CreationDeRep.Reponse("Bonjour")
     )
   }
-    @Test
+  @Test
     //Test avec uniquement salut sans autre requête
   def test_politesse_nil_2 {
+    CreationDeRep.action = 1
+    CreationDeRep.langues = 0
     assertEquals(
       List("Bonjour"),
       CreationDeRep.Reponse("Salut")
@@ -26,6 +70,8 @@ class TestCreationRep {
 
   @Test
   def test_nil {
+    CreationDeRep.action = 1
+    CreationDeRep.langues = 0
     assertEquals(
       List("Je ne comprends pas votre demande"),
       CreationDeRep.Reponse("")
@@ -35,6 +81,8 @@ class TestCreationRep {
   @Test
     //Test de requête sans politesse
   def test_requete {
+    CreationDeRep.action = 1
+    CreationDeRep.langues = 0
     assertEquals(
       List("L'adresse de Mairie de Rennes est : Place de la Mairie"),
       CreationDeRep.Reponse("Mairie de Rennes")
@@ -44,6 +92,8 @@ class TestCreationRep {
   @Test
     //Test avec requête et politesse
   def test_politesse_et_requete {
+    CreationDeRep.action = 1
+    CreationDeRep.langues = 0
     assertEquals(
       List("Bonjour","L'adresse de Mairie de Rennes est : Place de la Mairie"),
       CreationDeRep.Reponse("Bonjour Mairie de Rennes")
@@ -54,9 +104,21 @@ class TestCreationRep {
 @Test
     //Test avec une autre requête et politesse
   def test_politesse_et_requete_2 {
+    CreationDeRep.action = 1
+    CreationDeRep.langues = 0
     assertEquals(
       List("Bonjour","L'adresse de Théâtre National de Bretagne est : 1, Rue Saint-Hélier"),
       CreationDeRep.Reponse("Bonjour, je voudrais l'adresse du Théâtre National de Bretagne")
     )
   }
+  @Test
+    // Test avec une autre requête et politesse
+  def test_politesse_et_requete_introuvab {
+    CreationDeRep.action = 1
+    CreationDeRep.langues = 0
+    assertEquals(
+      List("Bonjour", "Je ne comprends pas votre demande"),
+      CreationDeRep.Reponse("Bonjour, 647hdc_8")
+)
+}
 }
